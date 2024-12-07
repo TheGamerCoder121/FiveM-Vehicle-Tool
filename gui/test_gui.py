1288443924ef0ebab20bdf36efa50cacb02d76c6
Nicklaus Vega <nicklaus.vega21@gmail.com>
2024-12-07 00:20:26 -0600

    Add test GUI implementation and command script for execution

--- gui/test_gui.py
+++
@@ -1,7 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QLabel
-import sys
-
-app = QApplication(sys.argv)
-label = QLabel("PyQt6 Test")
-label.show()
-sys.exit(app.exec())
---
+++ test_gui.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'test_gui.exe' with Python DLL being found.
+set PATH=c:\python311;%PATH%
+set PYTHONHOME=C:\Python311
+set NUITKA_PYTHONPATH=F:\Programming-Projects\Python\FiveM-Vehicle-Tool;C:\Python311\DLLs;C:\Python311\Lib;C:\Python311;F:\Programming-Projects\Python\FiveM-Vehicle-Tool\metatool;F:\Programming-Projects\Python\FiveM-Vehicle-Tool\metatool\Lib\site-packages;f:\programming-projects\python\fivem-vehicle-tool
+"%~dp0test_gui.exe" %*
